15f3ada5a527606bdc3abb45d9fd154a0df0e89f
mamaruf04 <mamar@DESKTOP-AR6EM10.(none)>
2023-02-17 23:37:58 +0600

    booked booking filtering and show it in the ui

--- index.js
+++ index.js
@@ -20,9 +20,26 @@ const run = async() => {
     try{
         const appointmentOptionCollections = client.db('doctorsDen').collection('appointmentOption');
 
+        const BookingsCollection = client.db('doctorsDen').collection('bookings');
+
         app.get('/appointmentOptions', async(req, res) =>{
+            const date = req.query.date;
             const query = {};
-            const result = await appointmentOptionCollections.find(query).toArray();
+            const options = await appointmentOptionCollections.find(query).toArray();
+            const bookingQuery = {appointmentDate : date};
+            const alreadyBooked = await BookingsCollection.find(bookingQuery).toArray();
+            options.forEach( option => {
+                const optionsBooked = alreadyBooked.filter(book => book.treatment === option.name);
+                const bookedSlot = optionsBooked.map(book => book.slot);
+                const remainingSlot = option.slots.filter(slot => !bookedSlot.includes(slot));
+                option.slots = remainingSlot;
+            })
+            res.send(options);
+        })
+
+        app.post('/bookings' , async(req,res) => {
+            const bookings = req.body;
+            const result = await BookingsCollection.insertOne(bookings);
             res.send(result);
         })
     }
